2143a08752a4b6afae2eff088caf44cac412d357
Ed Neal <edward2.neal@live.uwe.ac.uk>
2022-09-05 17:34:03 +0100

    Pushed Cubtron

---
+++ Assets/EdwardJosephNeal/README.md
@@ -0,0 +1,17 @@
+# Edward Joseph Neal - Cubotron
+
+#####WARNING Uses URP########
+
+Enter the Cubotron scene
+
+Press play
+
+When running press "space" to move the cubes.
+
+Inside the inspector you can:
+
+Edit the Cube Prefab script "cube data" to change the size and lifetime.
+
+Edit the Cube Spawner to change the amount of cubes and to change the size of the Cubotron / big cube.
+
+Edit the ExplosionAOE to change the key to move the cubes.
---
+++ Assets/EdwardJosephNeal/Runtime/Scripts/Explosion.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace EdwardJosephNeal
+{
+    public class Explosion : MonoBehaviour
+    {
+        public float m_ExplosionForce;
+        public float m_Radius;
+        public bool m_DrawRadius = true;
+        public KeyCode m_Detonate;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(m_Detonate))
+            {
+                Knockedback();
+            }
+        }
+
+        void Knockedback()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, m_Radius);
+
+            foreach (Collider nearby in colliders)
+            {
+                Rigidbody rb = nearby.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(m_ExplosionForce, transform.position, m_Radius);
+                }
+            }
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (m_DrawRadius)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(transform.position, m_Radius);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
